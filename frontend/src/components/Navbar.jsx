3e7083b3a0abeed6b7de38cc2723d79f02ec8428
Rasmus Henriksson <turboralle@hotmail.com>
2022-12-05 15:48:11 +0100

    :art: added some styling to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <nav className='navbar'>
         <div className="container flex">
             <ul className="flex navbar-nav">
+                <div className="fixxo-tag">Fixxo.</div>
                 <li><NavLink className="nav-link" to="/" end>Home</NavLink></li>
                 <li><NavLink className="nav-link" to="/blogg" >Categories</NavLink></li>
                 <li><NavLink className="nav-link" to="/Images" >Products</NavLink></li>
